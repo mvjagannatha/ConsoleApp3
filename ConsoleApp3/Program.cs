76bf5a31d467a71c77b83ffaf0a837add061b240
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 13:34:57 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Typeconversion {
+    class ExplicitCoversion {
+        static void Main(string[] args) {
+            double d = 5673.7;
+            int i;
+
+            i = (int)d;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
